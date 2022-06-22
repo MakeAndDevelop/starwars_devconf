5e134cee6cfc5272c3754a4e0dac699b867be8cf
MakeAndDevelop <ducofronik@gmail.com>
2022-06-23 00:04:05 +0200

    Revert "Update MainActivity.kt"
    This reverts commit 13151bd5152e1624d36ea3c3a39703f06a59d55f.

--- android/app/src/main/kotlin/com/example/starwars_devconf/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/starwars_devconf/MainActivity.kt
@@ -7,17 +7,21 @@ class MainActivity: FlutterActivity() {
     /// Configure flutter engine
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
             // This method is invoked on the main thread.
             call, result ->
             if (call.method == "getBatteryLevel") {
               val batteryLevel = getBatteryLevel()
-              result.success(batteryLevel)
+      
+              if (batteryLevel != -1) {
+                result.success(batteryLevel)
+              } else {
+                result.error("UNAVAILABLE", "Battery level not available.", null)
+              }
             } else {
               result.notImplemented()
             }
-        }
+          }
       }
 
       private fun getBatteryLevel(): Int {
